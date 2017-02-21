95bc293fbb863919ce0ce19d710fde1460c8d647
thiagopcunha <cunhazt@gmail.com>
2017-02-21 11:49:18 -0300

    Update novoPrato.php

--- controller/novoPrato.php
+++ controller/novoPrato.php
@@ -42,27 +42,29 @@
      <header>
 <body>
     <?php 
-      require_once ('../model/cardapio.php');
+      require_once ('../model/cardapio.php');  // local onde a classe se encontra
 
-      if(isset($_POST['nome'])){
+      if(isset($_POST['nome'])){  // Checha se há um pedido de insersão de pratos no cardapio
         $cardapio = new Cardapio;
 
-        if(isset($_FILES['imagem'])){
+        if(isset($_FILES['imagem'])){  // Checha se há a imagem do prato
           
           $extensao = strtolower(substr($_FILES['imagem']['name'], -4)); //pega extensao e converte pra minuscula
-          $novoNome = md5(time()) . $extensao;  //define o nome do arquivo evitando duplicação
+          $novoNome = md5(time()) . $extensao;  //define o nome do arquivo evitando duplicação utilizando a data
           $diretorio = "../view/image/cardapio/";  //define para onde vai o arquivo 
 
           move_uploaded_file($_FILES['imagem']['tmp_name'], $diretorio.$novoNome); //efetua o upload
 
           echo"imagem upada";
-
+            
+          
+        // seta os dados do prato e insere no banco de dados
         $cardapio->setPreco($_POST['preco']);
         $cardapio->setNome($_POST['nome']);
         $cardapio->setDescricao($_POST['descricao']);
         $cardapio->setImagem($novoNome);
 
-        $cardapio->inserirPrato();
+        $cardapio->inserirPrato();          
 
         echo '<p> Prato inserido com sucesso! </p>';
         echo '<a href="../view/administracao.php"> Voltar ao inicio </a>';
